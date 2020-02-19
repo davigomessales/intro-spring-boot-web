e296f3b58942584e19eed63c23a2f1314a336419
Davi Sales <davi.gomes.sales@gmail.com>
2020-02-19 09:04:08 -0300

    add application and configuration classes

--- src/main/java/br/com/devmedia/config/ConfiguracaoSpringMvc.java
+++ src/main/java/br/com/devmedia/config/ConfiguracaoSpringMvc.java
@@ -1,9 +1,24 @@
 package br.com.devmedia.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
 public class ConfiguracaoSpringMvc extends WebMvcConfigurerAdapter {
 
+    @Bean
+    public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver resolver){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(resolver);
+        return templateEngine;
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+    }
 }
